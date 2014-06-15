fb0702dc3f8e99e8b7441fb1557f027dc89c70ee
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-15 12:16:41 -0700

    added method to create animation from array.

--- Tappy Plane/TPPlane.m
+++ Tappy Plane/TPPlane.m
@@ -19,4 +19,23 @@
     return self;
 }
 
+-(SKAction*)animationFromArray:(NSArray*)textureNames withDuration:(CGFloat)duration
+{
+    // Create array to hold textures.
+    NSMutableArray *frames = [[NSMutableArray alloc] init];
+    // Get planes atlas.
+    SKTextureAtlas *planesAtlas = [SKTextureAtlas atlasNamed:@"Planes"];
+    // Loop through textureNames array and load textures.
+    for (NSString *textureName in textureNames) {
+        [frames addObject:[planesAtlas textureNamed:textureName]];
+    }
+    // Calculate time per frame.
+    CGFloat frameTime = duration / (CGFloat)frames.count;
+    // Create and return animation action.
+    return [SKAction repeatActionForever:[SKAction animateWithTextures:frames
+                                                          timePerFrame:frameTime
+                                                                resize:NO
+                                                               restore:NO]];
+}
+
 @end
