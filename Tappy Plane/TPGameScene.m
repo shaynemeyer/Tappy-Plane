506a311e60d3822f28cf98c44b558f8e393d8c52
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-15 11:45:40 -0700

    removed old planes and added Planes.atlas and added to project.

--- Tappy Plane/TPGameScene.m
+++ Tappy Plane/TPGameScene.m
@@ -13,7 +13,24 @@
 - (id)initWithSize:(CGSize)size
 {
     if (self = [super initWithSize:size]) {
-        NSLog(@"Size: %f %f", size.width, size.height);
+        
+        SKSpriteNode *plane1 = [SKSpriteNode spriteNodeWithImageNamed:@"planeBlue1@2x"];
+        plane1.position = CGPointMake(50, 50);
+        [self addChild:plane1];
+        
+        SKSpriteNode *plane2 = [SKSpriteNode spriteNodeWithImageNamed:@"planeRed1@2x"];
+        plane2.position = CGPointMake(100, 50);
+        [self addChild:plane2];
+        
+        SKSpriteNode *plane3 = [SKSpriteNode spriteNodeWithImageNamed:@"planeGreen1@2x"];
+        plane3.position = CGPointMake(150, 50);
+        [self addChild:plane3];
+        
+        SKSpriteNode *plane4 = [SKSpriteNode spriteNodeWithImageNamed:@"planeYellow1@2x"];
+        plane4.position = CGPointMake(200, 50);
+        [self addChild:plane4];
+        
+        
     }
     return self;
 }
