0f70961d37a63766f0bf37840aae8966f121c289
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-17 06:01:31 -0700

    added TPObstacleLayer class and mountain graphics.

---
+++ Tappy Plane/TPObstacleLayer.h
@@ -0,0 +1,13 @@
+//
+//  TPObstacleLayer.h
+//  Tappy Plane
+//
+//  Created by Shayne Meyer on 6/17/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+#import "TPScrollingNode.h"
+
+@interface TPObstacleLayer : TPScrollingNode
+
+@end
---
+++ Tappy Plane/TPObstacleLayer.m
@@ -0,0 +1,18 @@
+//
+//  TPObstacleLayer.m
+//  Tappy Plane
+//
+//  Created by Shayne Meyer on 6/17/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+#import "TPObstacleLayer.h"
+
+@implementation TPObstacleLayer
+
+-(void)updateWithTimeElapsed:(NSTimeInterval)timeElapsed
+{
+    [super updateWithTimeElapsed:timeElapsed];
+}
+
+@end
