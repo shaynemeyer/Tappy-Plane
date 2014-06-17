149f6685836eec01145d4ee6c0892d5c1f49c876
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-17 06:10:50 -0700

    added TPObstacleLayer class to represent the mountains in the game.

--- Tappy Plane/TPObstacleLayer.m
+++ Tappy Plane/TPObstacleLayer.m
@@ -8,11 +8,33 @@
 
 #import "TPObstacleLayer.h"
 
+@interface TPObstacleLayer ()
+
+@property (nonatomic) CGFloat marker;
+
+@end
+
+static const CGFloat kTPMarkerBuffer = 200.0;
+
 @implementation TPObstacleLayer
 
 -(void)updateWithTimeElapsed:(NSTimeInterval)timeElapsed
 {
     [super updateWithTimeElapsed:timeElapsed];
+    
+    if (self.scrolling && self.scene) {
+        // Find marker's location in scene coords.
+        CGPoint markerLocationInScene = [self convertPoint:CGPointMake(self.marker, 0) toNode:self.scene];
+        // When marker comes onto screen, add new obstacles.
+        if (markerLocationInScene.x - (self.scene.size.width * self.scene.anchorPoint.x) < self.scene.size.width + kTPMarkerBuffer) {
+            // if our marker is within 200 pixels from the right of the screen, add new obstacle.
+        }
+    }
+}
+
+-(void)addObstacleSet
+{
+    
 }
 
 @end
