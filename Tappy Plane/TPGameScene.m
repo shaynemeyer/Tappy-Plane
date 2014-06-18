c242a06f4283ea2980d2ab6fe9317d5a3d250bed
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-17 18:39:22 -0700

    add obstacles to game.

--- Tappy Plane/TPGameScene.m
+++ Tappy Plane/TPGameScene.m
@@ -10,6 +10,7 @@
 #import "TPPlane.h"
 #import "TPScrollingLayer.h"
 #import "TPConstants.h"
+#import "TPObstacleLayer.h"
 
 @interface TPGameScene ()
 
@@ -17,6 +18,7 @@
 @property (nonatomic) SKNode *world;
 @property (nonatomic) TPScrollingLayer *background;
 @property (nonatomic) TPScrollingLayer *foreground;
+@property (nonatomic) TPObstacleLayer *obstacles;
 @end
 
 static const CGFloat kMinFPS = 10.0 / 60.0;
@@ -51,6 +53,14 @@ static const CGFloat kMinFPS = 10.0 / 60.0;
         _background.horizontalScrollSpeed = -60;
         _background.scrolling = YES;
         [_world addChild: _background];
+        
+        // Setup obstacle layer.
+        _obstacles = [[TPObstacleLayer alloc] init];
+        _obstacles.horizontalScrollSpeed = -80;
+        _obstacles.scrolling = YES;
+        _obstacles.floor = 0.0;
+        _obstacles.ceiling = self.size.height;
+        [_world addChild:_obstacles];
         
         // Setup foreground.
         _foreground = [[TPScrollingLayer alloc] initWithTiles:@[[self generateGroundTile],
@@ -107,6 +117,11 @@ static const CGFloat kMinFPS = 10.0 / 60.0;
     // Reset layers.
     self.foreground.position = CGPointZero;
     [self.foreground layoutTiles];
+    
+    self.obstacles.position = CGPointZero;
+    [self.obstacles reset];
+    self.obstacles.scrolling = NO;
+    
     self.background.position = CGPointMake(0.0, 30);
     [self.background layoutTiles];
     
@@ -126,6 +141,7 @@ static const CGFloat kMinFPS = 10.0 / 60.0;
         } else {
             _player.physicsBody.affectedByGravity = YES;
             self.player.accelerating = YES;
+            self.obstacles.scrolling = YES;
         }
         
         
@@ -163,6 +179,7 @@ static const CGFloat kMinFPS = 10.0 / 60.0;
     if (!self.player.crashed) {
         [self.background updateWithTimeElapsed:timeElapsed];
         [self.foreground updateWithTimeElapsed:timeElapsed];
+        [self.obstacles updateWithTimeElapsed:timeElapsed];
     }
     
     
