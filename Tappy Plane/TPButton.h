15d89ea5d05d5a4cb4e3796685ed7d4c4c152842
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-21 15:47:08 -0700

    added class TPButton for custom button.

---
+++ Tappy Plane/TPButton.h
@@ -0,0 +1,15 @@
+//
+//  TPButton.h
+//  Tappy Plane
+//
+//  Created by Shayne Meyer on 6/21/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface TPButton : SKSpriteNode
+
+@property (nonatomic) CGFloat pressedScale;
+
+@end
---
+++ Tappy Plane/TPButton.m
@@ -0,0 +1,13 @@
+//
+//  TPButton.m
+//  Tappy Plane
+//
+//  Created by Shayne Meyer on 6/21/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+#import "TPButton.h"
+
+@implementation TPButton
+
+@end
