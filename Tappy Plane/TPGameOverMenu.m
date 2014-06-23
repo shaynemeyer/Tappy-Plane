80ba119583f579bf7ce770026d9e914a8fabe0cb
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-23 06:25:57 -0700

    setup scaling with CenterRect property.

--- Tappy Plane/TPGameOverMenu.m
+++ Tappy Plane/TPGameOverMenu.m
@@ -14,6 +14,17 @@
 {
     if (self = [super init]) {
         _size = size;
+        
+        SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:@"Graphics"];
+        SKSpriteNode *panelBackground = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"UIbg"]];
+        panelBackground.position = CGPointMake(size.width * 0.5, size.height - 150.0);
+        panelBackground.centerRect = CGRectMake(10 / panelBackground.size.width,
+                                                10 / panelBackground.size.height,
+                                                (panelBackground.size.width - 20) / panelBackground.size.width,
+                                                (panelBackground.size.height - 20) / panelBackground.size.height);
+        panelBackground.xScale = 175.0 / panelBackground.size.width;
+        panelBackground.yScale = 155.0 / panelBackground.size.height;
+        [self addChild:panelBackground];
     }
     return self;
 }
