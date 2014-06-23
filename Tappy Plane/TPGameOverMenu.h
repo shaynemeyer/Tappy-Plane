e8bb6b37101292d91e2e89f29db948d6e5b3a07f
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-23 06:16:13 -0700

    added game over class.

---
+++ Tappy Plane/TPGameOverMenu.h
@@ -0,0 +1,27 @@
+//
+//  TPGameOverMenu.h
+//  Tappy Plane
+//
+//  Created by Shayne Meyer on 6/23/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+typedef enum : NSUInteger {
+    MedalNone,
+    MedalBronze,
+    MedalSilver,
+    MedalGold,
+} MedalType;
+
+@interface TPGameOverMenu : SKNode
+
+@property (nonatomic) CGSize size;
+@property (nonatomic) NSInteger score;
+@property (nonatomic) NSInteger bestScore;
+@property (nonatomic) MedalType medal;
+
+-(instancetype)initWithSize:(CGSize)size;
+
+@end
---
+++ Tappy Plane/TPGameOverMenu.m
@@ -0,0 +1,21 @@
+//
+//  TPGameOverMenu.m
+//  Tappy Plane
+//
+//  Created by Shayne Meyer on 6/23/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+#import "TPGameOverMenu.h"
+
+@implementation TPGameOverMenu
+
+-(instancetype)initWithSize:(CGSize)size
+{
+    if (self = [super init]) {
+        _size = size;
+    }
+    return self;
+}
+
+@end
