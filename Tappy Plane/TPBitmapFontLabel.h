b45a6b0377ce0bb6c28cb05ed97e093f5679a996
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-24 05:45:33 -0700

    added text alignment to TPBitmapFontLabel.

--- Tappy Plane/TPBitmapFontLabel.h
+++ Tappy Plane/TPBitmapFontLabel.h
@@ -8,11 +8,18 @@
 
 #import <SpriteKit/SpriteKit.h>
 
+typedef enum : NSUInteger {
+    BitmapFontAlignmentLeft,
+    BitmapFontAlignmentCenter,
+    BitmapFontAlignmentRight,
+} BitmapFontAlignment;
+
 @interface TPBitmapFontLabel : SKNode
 
 @property (nonatomic) NSString *fontName;
 @property (nonatomic) NSString *text;
 @property (nonatomic) CGFloat letterSpacing;
+@property (nonatomic) BitmapFontAlignment alignment;
 
 -(instancetype)initWithText:(NSString*)text andFontName:(NSString*)fontName;
 
--- Tappy Plane/TPBitmapFontLabel.m
+++ Tappy Plane/TPBitmapFontLabel.m
@@ -16,6 +16,7 @@
         _text = text;
         _fontName = fontName;
         _letterSpacing = 2.0;
+        _alignment = BitmapFontAlignmentCenter;
         [self updateText];
     }
     return self;
@@ -41,6 +42,14 @@
 {
     if (_letterSpacing != letterSpacing) {
         _letterSpacing = letterSpacing;
+        [self updateText];
+    }
+}
+
+-(void)setAlignment:(BitmapFontAlignment)alignment
+{
+    if (_alignment != alignment) {
+        _alignment = alignment;
         [self updateText];
     }
 }
@@ -98,8 +107,23 @@
         totalSize.width -= self.letterSpacing;
     }
     
-    // Center text.
-    CGPoint adjustment = CGPointMake(-totalSize.width * 0.5, -totalSize.height * 0.5);
+    // Set text alignment.
+    CGPoint adjustment;
+    
+    switch (self.alignment) {
+        case BitmapFontAlignmentLeft:
+            adjustment = CGPointMake(0.0, -totalSize.height * 0.5);
+            break;
+        case BitmapFontAlignmentCenter:
+            adjustment = CGPointMake(-totalSize.width * 0.5, -totalSize.height * 0.5);
+            break;
+        case BitmapFontAlignmentRight:
+            adjustment = CGPointMake(-totalSize.width, -totalSize.height * 0.5);
+            break;
+        default:
+            break;
+    }
+    
     for (SKNode *letter in self.children) {
         letter.position = CGPointMake(letter.position.x + adjustment.x, letter.position.y + adjustment.y);
     }
