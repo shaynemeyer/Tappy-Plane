71b7621bd46f64a630d94279d0a0992d7053d6f5
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-26 06:15:55 -0700

    Add tap icons to get ready screen.

--- Tappy Plane/TPGetReadyMenu.m
+++ Tappy Plane/TPGetReadyMenu.m
@@ -11,6 +11,7 @@
 @interface TPGetReadyMenu()
 
 @property (nonatomic) SKSpriteNode *getReadyTitle;
+@property (nonatomic) SKNode *tapGroup;
 
 @end
 
@@ -21,11 +22,42 @@
     if (self = [self init]) {
         _size = size;
         
+        // Get graphics atlas
         SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:@"Graphics"];
-        
+        // Setup get ready text.
         _getReadyTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textGetReady"]];
         _getReadyTitle.position = CGPointMake(size.width * 0.75, planPosition.y);
         [self addChild:_getReadyTitle];
+        
+        // Setup group for tap related nodes.
+        _tapGroup = [SKNode node];
+        _tapGroup.position = planPosition;
+        [self addChild:_tapGroup];
+        
+        // Setup right tap tag.
+        SKSpriteNode *rightTapTag = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"tapLeft"]];
+        rightTapTag.position = CGPointMake(55, 0);
+        [self.tapGroup addChild:rightTapTag];
+        
+        // Setup left tap tag.
+        SKSpriteNode *leftTapTag = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"tapRight"]];
+        leftTapTag.position = CGPointMake(-55, 0);
+        [self.tapGroup addChild:leftTapTag];
+        
+        // Get frames for tap animation
+        NSArray *tapAnimationFrames = @[[atlas textureNamed:@"tap"],
+                                        [atlas textureNamed:@"tapTick"],
+                                        [atlas textureNamed:@"tapTick"]];
+        // Create action for tap animation
+        SKAction *tapAnimation = [SKAction animateWithTextures:tapAnimationFrames timePerFrame:0.5 resize:YES restore:NO];
+        
+        
+        // Setup tap hand.
+        SKSpriteNode *tapHand = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"tap"]];
+        tapHand.position = CGPointMake(0, -40);
+        [self.tapGroup addChild:tapHand];
+        [tapHand runAction:[SKAction repeatActionForever:tapAnimation]];
+        
     }
     return self;
 }
